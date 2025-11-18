ed2afe11f34369baf3372752285695ee91f5ce5a
mauro <73408974+holtzin29@users.noreply.github.com>
2025-11-17 21:58:06 -0300

    Add constantes.h with game constants definitions

---
+++ src/header/constantes.h
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#define UP 0 
+#define DOWN 1
+#define LEFT 2
+#define RIGTH 3
+
+
+#define SCREEN_WIDTH 800 // largura x
+#define SCREEN_HEIGTH 600 // comprimento y
+
+#define RECT_WIDTH 100.0f //largura retangulo
+#define RECT_HEIGTH 20.0f //comprimento  retangulo
+
+#define SPEED 200.0f // velocidade de movimentacao da barra
+
+#define RADIO 12
+
+#define MAX_BLOCOS 100
+
+#define MAX_JOGADORES 10
